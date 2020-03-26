8c4cb6e52cf8676b6e3a9ba71dcc1346e4949714
jerzyjeg <jerzyjeg@wp.pl>
2020-03-26 15:47:31 +0100

    LoDeviceProviderTest

--- src/test/java/com/orange/lo/sample/kerlink2lo/lo/LoDeviceProviderTest.java
+++ src/test/java/com/orange/lo/sample/kerlink2lo/lo/LoDeviceProviderTest.java
@@ -1,36 +1,44 @@
 package com.orange.lo.sample.kerlink2lo.lo;
 
 import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.MockRestServiceServer;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
+@RestClientTest(LoDeviceProvider.class)
 @SpringBootTest
 public class LoDeviceProviderTest {
 
     @Autowired
     LoDeviceProvider loDeviceProvider;
 
-//    @Autowired
-//    private LoProperties loProperties;
-//
-//    @Autowired
-//    private MockRestServiceServer server;
-//
-//    @Autowired
-//    private ObjectMapper objectMapper;
+    @Autowired
+    private LoProperties loProperties;
+
+    @Autowired
+    private MockRestServiceServer server;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Before
     public void setUp() throws Exception {
-//        this.loDeviceProvider = new LoDeviceProvider(loProperties, new HttpHeaders());
-        //String loDeviceString = objectMapper.writeValueAsString(new LoDevice("urn:lo:nsid:x-connector:0018B20000002345", null, null, true));
-        
-        //server.expect(requestTo("https://liveobjects.orange-business.com/api/v1/deviceMgt/devices")).andRespond(withSuccess(loDeviceString, MediaType.APPLICATION_JSON));
+        String loDeviceString = objectMapper.writeValueAsString(
+                new LoDevice("urn:lo:nsid:x-connector:0018B20000002345", null, null, true));
+        server.expect(requestTo(
+                "https://liveobjects.orange-business.com/api/v1/deviceMgt/devices")).andRespond(
+                        withSuccess(loDeviceString, MediaType.APPLICATION_JSON));
     }
 
     @Test
