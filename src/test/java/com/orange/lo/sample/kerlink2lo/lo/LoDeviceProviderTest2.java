6443067de49efa2747be203a6fcdd74dbdb4f3a2
jerzyjeg <jerzyjeg@wp.pl>
2020-04-06 10:12:57 +0200

    testing getDevices, addDevice, deleteDevice

--- src/test/java/com/orange/lo/sample/kerlink2lo/lo/LoDeviceProviderTest2.java
+++ src/test/java/com/orange/lo/sample/kerlink2lo/lo/LoDeviceProviderTest2.java
@@ -6,93 +6,175 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.*;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.http.*;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.any;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @RunWith(MockitoJUnitRunner.class)
 @RestClientTest(LoDeviceProvider.class)
 public class LoDeviceProviderTest2 {
 
-    LoDeviceProvider loDeviceProvider;
+    @Mock
+    LoProperties loProperties;
 
     @Mock
     RestTemplate restTemplate;
 
+    @Mock
+    HttpHeaders httpHeaders;
+
     @Before
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        LoProperties loProperties = new LoProperties();
-        HttpHeaders httpHeaders = null;
-        loProperties.setApiUrl("localhost/v1/deviceMgt/devices?limit=2&offset=0&groupId=null&fields=id,name,group");
-        loProperties.setPageSize(20);
-        loProperties.setDeviceGroupId("kerlink");
-        loDeviceProvider = new LoDeviceProvider(loProperties, httpHeaders, restTemplate);
-    }
-
-    @Test
-    public void shouldGet4Devices() {
-        LoDevice[] devices = new LoDevice[] {new LoDevice(), new LoDevice()};
-        ResponseEntity<LoDevice[]> response = new ResponseEntity<LoDevice[]>(devices, getHeadersWithRemainingAndReset(4, 3, 3), HttpStatus.OK);
-        when(restTemplate.exchange(anyString(), eq(HttpMethod.GET), anyObject(), eq(LoDevice[].class)))
-                .thenReturn(response);
-        int count = loDeviceProvider.getDevices().size();
-        Assert.assertEquals(4, count);
+        when(loProperties.getApiUrl()).thenReturn("localhost");
     }
 
     @Test
     public void shouldGetNoDevices() {
-        LoDevice[] devices = new LoDevice[0];
-        ResponseEntity<LoDevice[]> response = new ResponseEntity<LoDevice[]>(devices, HttpStatus.OK);
-        when(restTemplate.exchange(anyString(), eq(HttpMethod.GET), anyObject(), eq(LoDevice[].class)))
-                .thenReturn(response);
-        int devicesCount = loDeviceProvider.getDevices().size();
-        Assert.assertEquals(0, devicesCount);
+        when(restTemplate.exchange(anyString(), eq(HttpMethod.GET), ArgumentMatchers.any(), eq(LoDevice[].class)))
+                .thenReturn(getResponse(0, 0, 0, 0));
+        int count = new LoDeviceProvider(loProperties, httpHeaders, restTemplate).getDevices().size();
+        Assert.assertEquals(0, count);
     }
 
     @Test
-    public void shouldGetTotalCountDevices() {
-        int totalCount = 5;
-        LoDevice[] devices = new LoDevice[totalCount];
-        for (int i = 0;  i < totalCount; i++)
+    public void shouldGetNoDevicesWitLimitZero() {
+        int limit = 0;
+        int total = 1;
+        when(loProperties.getPageSize()).thenReturn(limit);
+        when(restTemplate.exchange(eq(getUrl(limit, 0)), eq(HttpMethod.GET),
+                ArgumentMatchers.any(), eq(LoDevice[].class))).thenReturn(
+                getResponse(limit, total, 1, 0));
+        int count = new LoDeviceProvider(loProperties, httpHeaders, restTemplate).getDevices().size();
+        Assert.assertEquals(limit, count);
+    }
+
+    @Test
+    public void shouldGetDevicesWithLimitEqualsTotalCount() {
+        int limit = 2;
+        when(loProperties.getPageSize()).thenReturn(limit);
+        when(restTemplate.exchange(eq(getUrl(limit, 0)), eq(HttpMethod.GET),
+                ArgumentMatchers.any(), eq(LoDevice[].class))).thenReturn(
+                getResponse(limit, limit, 1, 0));
+        int count = new LoDeviceProvider(loProperties, httpHeaders, restTemplate).getDevices().size();
+        Assert.assertEquals(limit, count);
+    }
+
+    @Test
+    public void shouldGetDevicesWithLimitGreaterThanTotalCount() {
+        int limit = 3;
+        int total = 2;
+        when(loProperties.getPageSize()).thenReturn(limit);
+        when(restTemplate.exchange(eq(getUrl(limit, 0)), eq(HttpMethod.GET),
+                ArgumentMatchers.any(), eq(LoDevice[].class))).thenReturn(
+                getResponse(total, total, 1, 0));
+        int count = new LoDeviceProvider(loProperties, httpHeaders, restTemplate).getDevices().size();
+        Assert.assertEquals(total, count);
+    }
+
+    @Test
+    public void shouldGetDevicesWithLimitUndercostamOfTotalCount() {
+        int limit = 2;
+        int total = 4;
+        when(loProperties.getPageSize()).thenReturn(limit);
+        when(restTemplate.exchange(eq(getUrl(limit, 0)), eq(HttpMethod.GET),
+                ArgumentMatchers.any(), eq(LoDevice[].class))).thenReturn(
+                        getResponse(limit, total, 1, 0));
+        when(restTemplate.exchange(eq(getUrl(limit, 2)), eq(HttpMethod.GET),
+                ArgumentMatchers.any(), eq(LoDevice[].class))).thenReturn(
+                        getResponse(limit, total, 1, 0));
+        int count = new LoDeviceProvider(loProperties, httpHeaders, restTemplate).getDevices().size();
+        Assert.assertEquals(total, count);
+    }
+
+    @Test
+    public void shouldGetDevicesWithLimitNotUndercostamOfTotalCount() {
+        int limit = 2;
+        int total = 3;
+        when(loProperties.getPageSize()).thenReturn(limit);
+        when(restTemplate.exchange(eq(getUrl(limit, 0)), eq(HttpMethod.GET),
+                ArgumentMatchers.any(), eq(LoDevice[].class))).thenReturn(
+                getResponse(limit, total, 1, 0));
+        when(restTemplate.exchange(eq(getUrl(limit, 2)), eq(HttpMethod.GET),
+                ArgumentMatchers.any(), eq(LoDevice[].class))).thenReturn(
+                getResponse(total - limit, total, 1, 0));
+        int count = new LoDeviceProvider(loProperties, httpHeaders, restTemplate).getDevices().size();
+        Assert.assertEquals(total, count);
+    }
+
+    @Test
+    public void shouldGetDevicesOfQuantityGreaterThanTotalCount() {
+        int limit = 2;
+        int total = 3;
+        when(loProperties.getPageSize()).thenReturn(limit);
+        when(restTemplate.exchange(eq(getUrl(limit, 0)), eq(HttpMethod.GET),
+                ArgumentMatchers.any(), eq(LoDevice[].class))).thenReturn(
+                getResponse(limit, total, 1, 0));
+        when(restTemplate.exchange(eq(getUrl(limit, 2)), eq(HttpMethod.GET),
+                ArgumentMatchers.any(), eq(LoDevice[].class))).thenReturn(
+                getResponse(limit, total, 1, 0));
+        int count = new LoDeviceProvider(loProperties, httpHeaders, restTemplate).getDevices().size();
+        Assert.assertEquals(2 * limit, count);
+    }
+
+    @Test
+    public void shouldGetDevicesAfterSleep() {
+        int limit = 2;
+        int total = 3;
+        when(loProperties.getPageSize()).thenReturn(limit);
+        when(restTemplate.exchange(eq(getUrl(limit, 0)), eq(HttpMethod.GET),
+                ArgumentMatchers.any(), eq(LoDevice[].class))).thenReturn(
+                getResponse(limit, total, 0, System.currentTimeMillis() + 10000));
+        when(restTemplate.exchange(eq(getUrl(limit, 2)), eq(HttpMethod.GET),
+                ArgumentMatchers.any(), eq(LoDevice[].class))).thenReturn(
+                getResponse(limit, total, 1, 0));
+        long start = System.currentTimeMillis();
+        new LoDeviceProvider(loProperties, httpHeaders, restTemplate).getDevices();
+        long stop = System.currentTimeMillis();
+        Assert.assertTrue(stop - start >= 10000);
+    }
+
+    @Test
+    public void shouldAddDevice() {
+        String deviceId = "test";
+        when(loProperties.getPageSize()).thenReturn(0);
+        when(loProperties.getDeviceGroupId()).thenReturn("kerlink");
+        when(loProperties.getDevicePrefix()).thenReturn("urn:lo:nsid:x-connector:");
+        when(restTemplate.exchange(eq("localhost/v1/deviceMgt/devices"), eq(HttpMethod.POST),
+                ArgumentMatchers.<HttpEntity<LoDevice>>any(), eq(Void.class))).thenReturn(new ResponseEntity<>(HttpStatus.OK));
+        new LoDeviceProvider(loProperties, httpHeaders, restTemplate).addDevice(deviceId);
+    }
+
+    @Test
+    public void shouldDeleteDevice() {
+        String deviceId = "test";
+        when(loProperties.getPageSize()).thenReturn(0);
+        when(restTemplate.exchange(eq("localhost/v1/deviceMgt/devices/" + deviceId), eq(HttpMethod.DELETE),
+                ArgumentMatchers.any(), eq(Void.class))).thenReturn(new ResponseEntity<>(HttpStatus.OK));
+        new LoDeviceProvider(loProperties, httpHeaders, restTemplate).deleteDevice(deviceId);
+    }
+
+    private String getUrl(int limit, int offset) {
+        return "localhost/v1/deviceMgt/devices?limit=" + limit +
+                "&offset=" + offset + "&groupId=null&fields=id,name,group";
+    }
+
+    private ResponseEntity<LoDevice[]> getResponse(int devicesCount, int xTotalCount, int xRateLimitRemaining, long xRateLimitReset) {
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        headers.add("X-Total-Count", String.valueOf(xTotalCount));
+        headers.add("X-Ratelimit-Remaining", String.valueOf(xRateLimitRemaining));
+        headers.add("X-Ratelimit-Reset", String.valueOf(xRateLimitReset));
+        return new ResponseEntity<>(returnDevices(devicesCount), headers, HttpStatus.OK);
+    }
+
+    private LoDevice[] returnDevices(int count) {
+        LoDevice[] devices = new LoDevice[count];
+        for (int i = 0;  i < count; i++)
             devices[i] = new LoDevice();
-        ResponseEntity<LoDevice[]> response = new ResponseEntity<>(devices, getHeadersWithTotalCount(totalCount), HttpStatus.OK);
-        when(restTemplate.exchange(anyString(), eq(HttpMethod.GET), anyObject(), eq(LoDevice[].class)))
-                .thenReturn(response);
-        int devicesCount = loDeviceProvider.getDevices().size();
-        Assert.assertEquals(totalCount, devicesCount);
+        return devices;
     }
-
-    @Test
-    public void shouldSleep() {
-        int totalCount = 5;
-        int count = 3;
-        String remain = "0";
-    }
-
-    private MultiValueMap<String, String> getHeadersWithTotalCount(long count) {
-        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-        headers.add("X-Total-Count", String.valueOf(count));
-        return headers;
-    }
-
-    private MultiValueMap<String, String> getHeadersWithRemainingAndReset(long count, long remaining, long reset) {
-        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-        headers.add("X-Total-Count", String.valueOf(count));
-        headers.add("X-Ratelimit-Remaining", String.valueOf(remaining));
-        headers.add("X-Ratelimit-Reset", String.valueOf(reset));
-        return headers;
-    }
-
-
 }
